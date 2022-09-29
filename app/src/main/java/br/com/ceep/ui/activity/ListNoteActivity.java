661be3d2f57374430a9e7f6bc33186fab9d8dbc5
Roberto Rosa <roberto.rosa7@gmail.com>
2022-09-29 18:49:54 -0300

    android ceep - fisrt commit

---
+++ app/src/main/java/br/com/ceep/ui/activity/ListNoteActivity.java
@@ -0,0 +1,14 @@
+package br.com.ceep.ui.activity;
+
+import android.os.Bundle;
+
+import br.com.ceep.R;
+
+public class ListNoteActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_note);
+	}
+}
---
+++ app/src/main/java/br/com/ceep/ui/activity/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
